98e7eb0b79de96dd3842e411520a3a41b820e29b
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-09-20 01:30:59 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# update-image-src-with-button-click-vue-3-example
-Created with CodeSandbox
+# Vue 3 change image url with button click example
+
+Learn how to change the URL / src of an image with a button click in Vue 3 by dynamically updating the src URL with an example.
+[Learn more](https://www.nightprogrammer.com/vue-js/how-to-update-src-url-of-an-image-with-a-button-click-in-vue-3-example/)
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h3>Vue 3 change image URL with button click</h3>
+    <button @click="showPreviousImage">Previous</button>
+    <button @click="showNextImage">Next</button>
+    <p>Image URL: {{ imageUrl }}</p>
+    <div class="np-ht">
+      <img :src="imageUrl" />
+    </div>
+    <!--
+      www.nightprogrammer.com
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      imageId: 1,
+    };
+  },
+  computed: {
+    imageUrl() {
+      return `https://picsum.photos/id/${this.imageId}/500/400`;
+    },
+  },
+  methods: {
+    showPreviousImage() {
+      if (this.imageId > 1) this.imageId--;
+    },
+    showNextImage() {
+      this.imageId++;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  border: 1px solid #000;
+  min-width: 120px;
+  margin: 10px;
+  padding: 6px;
+}
+</style>
